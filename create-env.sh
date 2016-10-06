25ddf5a623652376a2ec16e51bee82aaaf752438
arianitp <arianitpajaziti@gmail.com>
2016-10-05 22:39:43 -0500

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -5,7 +5,7 @@ if [ $# != 1 ]; then
 else
 	echo "Creating Load Balancer, LoadBalancer Policy"
 	aws elb create-load-balancer --load-balancer-name loadBalancer --listeners Protocol=http,LoadBalancerPort=80,InstanceProtocol=http,InstancePort=80 --availability-zones "us-west-2b"
-	aws elb create-load-balancer-policy --load-balancer-name loadBalancer --policy-name balancerPolicy --policy-type-name proxyProtPolicy --policy-attributes AttributeName=ProxyProtocol,AttributeValue=true
+	aws elb create-load-balancer-policy --load-balancer-name loadBalancer --policy-name nxtGenBalancerPolicy --policy-type-name ProxyProtocolPolicyType --policy-attributes AttributeName=ProxyProtocol,AttributeValue=true
 	echo "Configuring Launch Policy and Autoscale Group"
 	aws autoscaling create-launch-configuration --launch-configuration-name web_server --image-id $1 --key-name ec2KeyMac --security-groups sg-a1208fd8 --instance-type t2.micro --user-data file://installenv.sh --placement AvailabilityZone=us-west-2b --placement-tenancy default
 	aws autoscaling create-auto-scaling-group --launch-configuration-name web_server --auto-scaling-group-name autoScaleGroup --min-size 2 --max-size 5 --desired-capacity 4 --availability-zones "us-west-2b"
